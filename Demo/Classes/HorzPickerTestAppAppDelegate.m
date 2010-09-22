c8c2b2dd98f6cc2142e2f5f9ccf9310476ede9e5
Shawn Veader <shawn@veader.org>
2010-09-21 20:18:53 -0400

    boiler plate 'fun'

--- Demo/Classes/HorzPickerTestAppAppDelegate.m
+++ Demo/Classes/HorzPickerTestAppAppDelegate.m
@@ -3,7 +3,7 @@
 //  HorzPickerTestApp
 //
 //  Created by Shawn Veader on 9/20/10.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//  Copyright 2010 V8 Labs, LLC. All rights reserved.
 //
 
 #import "TestViewController.h"
